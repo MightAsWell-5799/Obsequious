3f2cb4cfa78b1a7b55d9c55283afd392d6530a57
MightAsWell-5799 <sebhase@gmail.com>
2021-03-27 20:56:14 -0400

    done for the day

--- Website/OAuth2/1/index.js
+++ Website/OAuth2/1/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 
-var mongoose = require('mongoose')
+var mongoose = require("mongoose");
 
-
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'))
 
 
 app.get("/", (req, res) => {
-	res.send("hello");
+	res.render("main/home", {});
+
 });
 
 app.listen(80);
---
+++ Website/OAuth2/1/routes/router.js
@@ -0,0 +1,7 @@
+const uuid = require('uuid')
+const http = require('http')
+const express = require('express')
+const fs = require('fs')
+
+
+const router = express.Router()
