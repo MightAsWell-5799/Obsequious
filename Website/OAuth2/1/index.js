d19475cd925ee4cdcce67c58ce47f2bf05f40bc8
MightAsWell-5799 <sebhase@gmail.com>
2021-03-28 14:55:39 -0400

    extended website styling and pushed pages

--- Website/OAuth2/1/index.js
+++ Website/OAuth2/1/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const authRouter = require('./routes/authrouter')
 const app = express()
 
 var mongoose = require('mongoose')
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
-
 app.use(express.static(__dirname + '/public'))
+ 
+app.use("/auth", authRouter)
+
+
+
 
 app.get('/', (req, res) => {
     res.render('main/home', {})
--- Website/OAuth2/1/routes/authrouter.js
+++ Website/OAuth2/1/routes/authrouter.js
@@ -4,3 +4,23 @@ const express = require('express')
 const fs = require('fs')
 
 const router = express.Router()
+
+//?login page
+router.get('/login', (req, res)=>{
+    res.render('auth/login')
+})
+
+//logout
+router.get('/logout', (req, res)=>{
+    //passport
+    res.send("logged out")
+})
+
+
+//! auth with discord
+router.get('/discord', (req, res)=>{
+    //passport
+    res.send("discord login")
+})
+
+module.exports = router
